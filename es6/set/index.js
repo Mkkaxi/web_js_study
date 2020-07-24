0ec701518276c5509324256ed987f11568064cb8
Mkkaxi <1641883046@qq.com>
2020-07-24 11:07:02 +0800

    s

---
+++ es6/set/index.js
@@ -0,0 +1,37 @@
+
+
+const s = new Set()   // 类数组
+// const arr = new Array()
+
+
+ 
+
+//  for in 不能用来遍历数组 ！！！！！！！！
+
+// for (let i of arr) {
+//   console.log(i);
+// }
+
+// arr.forEach((x) => {
+//   // console.log(x);
+//   s.add(x)
+  
+// })
+// console.log(s);
+
+// const set = new Set([1,2,3,4,4,4,3,2,1])
+// const set = new Set()
+// console.log(set);
+// console.log(set.size);
+
+// // 数组去重
+// let arr =  [2, 3, 4, 1, 3, 2, 5]
+// console.log([...new Set(arr)])
+
+// // 字符串去重
+// console.log([...new Set('sssafga')])
+
+// let a = NaN
+// let b = NaN
+// set.add(a)
+// set.add(b)
