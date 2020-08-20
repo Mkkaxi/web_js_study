69519127eab00d8b0bb8bdfeeeecf5f35fc72314
Mkkaxi <1641883046@qq.com>
2020-08-20 11:30:12 +0800

    typeScript

---
+++ ts/hello-ts/readme.md
@@ -0,0 +1,3 @@
+1. 安装 ts
+2. tsc --init
+3. 编辑 tsconfig.json
---
+++ ts/hello-ts/src/index.ts
@@ -0,0 +1,36 @@
+function greeter(person: string) {
+  return "hello" + person
+}
+
+
+const user = 'ww'
+
+
+// 类型 
+// let isLoading: boolean = false
+// isLoading = 123 
+
+// let num = 6
+// num = '78'
+
+
+// let book: string = 'typeScript'
+
+
+// 空值
+// function warn(): void {
+//   return 1
+// }
+
+
+const a: void = undefined
+
+let aa: any = undefined
+
+let aaa: undefined = undefined
+
+let symbol: symbol = Symbol('key')
+let symbol2: symbol = Symbol('key')
+
+
+// BigInt
