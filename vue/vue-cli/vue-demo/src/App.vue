1f23dd9adf84aad68674d94d43f132d5e7a7154a
Mkkaxi <1641883046@qq.com>
2020-08-24 17:02:20 +0800

    s

---
+++ vue/vue-cli/vue-demo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <home></home>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue-cli/vue-demo/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <p class="name">wn</p>
+    <p class="age">18</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
