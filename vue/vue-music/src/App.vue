40b00adaf715f6fc546b935fc8bb73dc8d40a175
Mkkaxi <1641883046@qq.com>
2020-09-07 22:43:24 +0800

    vue-music

---
+++ vue/vue-music/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+import header from '@/components/header'
+export default {
+  components: {
+    'v-header': header
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
