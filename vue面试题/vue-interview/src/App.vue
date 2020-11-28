8b3281a276ade82f9dd48cbe2010f60a7c7449ca
Mkkaxi <1641883046@qq.com>
2020-11-28 22:39:09 +0800

    s

---
+++ vue面试题/vue-interview/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  beforeCreate() {
+    console.log('APP beforeCreate');
+  },
+  Created() {
+    console.log('APP Created');
+  },
+  beforeMount() {
+    console.log('APP beforeMount');
+  },
+  mounted() {
+    console.log('APP Mounted');
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue面试题/vue-interview/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  beforeCreate() {
+    console.log('son beforeCreate');
+  },
+  Created() {
+    console.log('son Created');
+  },
+  beforeMount() {
+    console.log('son beforeMount');
+  },
+  mounted() {
+    console.log('son Mounted');
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
